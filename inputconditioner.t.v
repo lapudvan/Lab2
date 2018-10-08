f2d25112e6bf5bc7e759c478c272b7d4da465358
vivienyuwenchen <vivienyuwenchen@gmail.com>
2018-10-08 19:44:07 -0400

    Complete inputconditioner

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -1,6 +1,8 @@
 //------------------------------------------------------------------------
 // Input Conditioner test bench
 //------------------------------------------------------------------------
+`timescale 1 ns / 1 ps
+`include "inputconditioner.v"
 
 module testConditioner();
 
@@ -9,21 +11,35 @@ module testConditioner();
     wire conditioned;
     wire rising;
     wire falling;
-    
+
     inputconditioner dut(.clk(clk),
     			 .noisysignal(pin),
 			 .conditioned(conditioned),
 			 .positiveedge(rising),
-			 .negativeedge(falling))
+			 .negativeedge(falling));
 
 
     // Generate clock (50MHz)
     initial clk=0;
     always #10 clk=!clk;    // 50MHz Clock
-    
+
     initial begin
     // Your Test Code
     // Be sure to test each of the three conditioner functions:
     // Synchronization, Debouncing, Edge Detection
-    
+    // 200 nanoseconds
+    $dumpfile("inputconditioner.vcd");
+    $dumpvars();
+    pin=0; #200
+    pin=1; #25
+    pin=0; #200
+    pin=1; #200
+    pin=0; #25
+    pin=1; #200
+    pin=0; #200
+    // if(conditioned != 1'b0)
+    // $display("ADD test 1 - carryout: %h, expected: 0", carryout);
+    // end
+    $finish();
+    end
 endmodule
--- inputconditioner.v
+++ inputconditioner.v
@@ -9,27 +9,41 @@ module inputconditioner
 (
 input 	    clk,            // Clock domain to synchronize input to
 input	    noisysignal,    // (Potentially) noisy input signal
-output reg  conditioned,    // Conditioned output signal
-output reg  positiveedge,   // 1 clk pulse at rising edge of conditioned
-output reg  negativeedge    // 1 clk pulse at falling edge of conditioned
+output reg  conditioned = 0,    // Conditioned output signal
+output reg  positiveedge = 0,   // 1 clk pulse at rising edge of conditioned
+output reg  negativeedge = 0    // 1 clk pulse at falling edge of conditioned
 );
+
+/*Modify the module so that the positiveedge and negativeedge output signals
+are correctly generated. These signals should be high for exactly one clock
+period when conditioned has a positive/negative edge, starting in the same
+clock period that conditioned transitions.
+
+Note: There are several possible ways to generate the edge signals.
+Remember that assign statements are continuous and operate on wires,
+while assignments in always blocks (e.g. nonblocking <=) operate on regs.
+*/
 
     parameter counterwidth = 3; // Counter size, in bits, >= log2(waittime)
     parameter waittime = 3;     // Debounce delay, in clock cycles
-    
+
     reg[counterwidth-1:0] counter = 0;
     reg synchronizer0 = 0;
     reg synchronizer1 = 0;
-    
-    always @(posedge clk ) begin
+
+    always @(posedge clk) begin
+        positiveedge <= 0;
+        negativeedge <= 0;
         if(conditioned == synchronizer1)
             counter <= 0;
         else begin
             if( counter == waittime) begin
                 counter <= 0;
                 conditioned <= synchronizer1;
+                positiveedge <= synchronizer1;
+                negativeedge <= !synchronizer1;
             end
-            else 
+            else
                 counter <= counter+1;
         end
         synchronizer0 <= noisysignal;
